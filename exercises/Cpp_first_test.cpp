fb1dfc1877c9f5fa042bc88245e03946b853d407
Miguel Isack verdadeiro <130250696+MiguelSouza2@users.noreply.github.com>
2024-07-04 16:26:50 -0300

    Add files via upload

---
+++ exercises/Cpp_first_test.cpp
@@ -0,0 +1,22 @@
+// INCLUSÃO DA BIBLIOTECA <IOSTREAM>  --> IO = IN OUT = ENTRADA E SAÍDA
+
+#include <iostream>
+
+// SEPARA EM PASTA (STD)
+
+using namespace std;
+
+// CORPO DO PROJETO (MAIN)
+
+int main(){
+	// COUT MOSTRA A MSG NA TELA; << É O QUE SERÁ MOSTRADO; ENDL É QUEBRA DE LINHA;
+	cout << "ola mundinho" << endl;
+	system("pause");
+	
+	// \N QUEBRA A LINHA TAMBÉM
+	cout << "esse texto usou barra n\n";
+	system("pause");
+	
+	// O QUE A FUNÇÃO VAI RETORNAR (0)
+	return 0;
+}
